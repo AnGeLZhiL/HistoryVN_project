4841aecd95a630507c59d6f2670cdc0cbb00df65
AnGeLZhiL <angelina.zhilyakova.2002@mail.ru>
2023-05-01 23:05:02 +0300

    Создана навигация

--- app/src/main/java/com/example/historyvn_project/MainActivity.kt
+++ app/src/main/java/com/example/historyvn_project/MainActivity.kt
@@ -2,11 +2,25 @@ package com.example.historyvn_project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.historyvn_project.common.Global
+import com.example.historyvn_project.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment =supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController =navHostFragment.navController
+        setupWithNavController(binding.bottomNavigationView, navController)
     }
 }
